c6a582e3dee76519af34dd2300293d432a366481
tsazo <tsazo@andover.edu>
2022-07-15 17:50:55 -0400

    Add template

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,83 +1,5 @@
 import * as React from "react"
-import { Link } from 'gatsby'
-
-// styles
-const pageStyles = {
-  color: "#232129",
-  padding: 96,
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
-const headingAccentStyles = {
-  color: "#663399",
-}
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
-const listStyles = {
-  marginBottom: 96,
-  paddingLeft: 0,
-}
-const listItemStyles = {
-  fontWeight: 300,
-  fontSize: 24,
-  maxWidth: 560,
-  marginBottom: 30,
-}
-
-const linkStyle = {
-  color: "#8954A8",
-  fontWeight: "bold",
-  fontSize: 16,
-  verticalAlign: "5%",
-}
-
-const docLinkStyle = {
-  ...linkStyle,
-  listStyleType: "none",
-  marginBottom: 24,
-}
-
-const descriptionStyle = {
-  color: "#232129",
-  fontSize: 14,
-  marginTop: 10,
-  marginBottom: 0,
-  lineHeight: 1.25,
-}
-
-const docLink = {
-  text: "Documentation",
-  url: "https://www.gatsbyjs.com/docs/",
-  color: "#8954A8",
-}
-
-const badgeStyle = {
-  color: "#fff",
-  backgroundColor: "#088413",
-  border: "1px solid #088413",
-  fontSize: 11,
-  fontWeight: "bold",
-  letterSpacing: 1,
-  borderRadius: 4,
-  padding: "4px 6px",
-  display: "inline-block",
-  position: "relative",
-  top: -2,
-  marginLeft: 10,
-  lineHeight: 1,
-}
+import Layout from '../components/layout'
 
 // data
 const links = [
@@ -129,19 +51,23 @@ const links = [
 // markup
 const IndexPage = () => {
   return (
-    <main style={pageStyles}>
-      <title>Home Page</title>
-      <h1 style={headingStyles}>
-        Welcome to my Gatsby site!
-      </h1>
-      <Link to="/about">About</Link>
-      <p style={paragraphStyles}>
-        I'm making this by following the Gatsby Tutorial.
-        <span role="img" aria-label="Sunglasses smiley emoji">
-          😎
-        </span>
-      </p>
-    </main>
+    // <main style={pageStyles}>
+    //   <title>Home Page</title>
+    //   <h1 style={headingStyles}>
+    //     Welcome to my Gatsby site!
+    //   </h1>
+    //   <Link to="/about">About</Link>
+    //   <p style={paragraphStyles}>
+    //     I'm making this by following the Gatsby Tutorial.
+    //     <span role="img" aria-label="Sunglasses smiley emoji">
+    //       😎
+    //     </span>
+    //   </p>
+    // </main>
+
+    <Layout pageTitle="Home Page">
+      <p>I'm making this by following the Gatsby Tutorial.</p>
+    </Layout>
   )
 }
 
